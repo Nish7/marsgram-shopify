308ea41d27215e1f60b43eb31c128cda7f557054
Nishil K <nishilkapadia17@gmail.com>
2022-01-20 02:36:13 +0530

    feed link fixed

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Project submittion for shopify internship in summer 2022
 
 
 ### Features
-- Ability to like the posts
+- Ability to like the posts and persist them (in localstorage)
 - Dark and Light mode
 - Search images by date
 - Loading state and exception handling
--- pages/api/feed.js
+++ pages/api/feed.js
@@ -4,14 +4,14 @@ export default async function handler(req, res) {
 	try {
 		const { date } = req.query;
 		const { data: rovers } = await axios.get(
-			'http://localhost:3000/api/rovers'
+			'https://marsgram.vercel.app/api/rovers'
 		);
 
 		let feed = [];
 
 		for (const r of rovers) {
 			const { data: r_photos } = await axios.get(
-				`http://localhost:3000/api/photos?rover=${r.name}&date=${date}`
+				`https://marsgram.vercel.app/api/photos?rover=${r.name}&date=${date}`
 			);
 
 			feed.push(...r_photos);
